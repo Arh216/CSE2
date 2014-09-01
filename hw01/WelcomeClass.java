6d719a7603644f4bbbb8c0ee6deb602598f998d6
arh216 <arh216@lehigh.edu>
2014-09-01 16:51:24 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Ariana Hewitt
+//CSE002
+//September 1, 2014
+//Hw1
+//When program is run, it prints out "Welcome" then my username, arh216 and then
+//a 140 character biography
+public class WelcomeClass {
+     //this defines a class
+     public static void main(String[] args) {
+          //this defines a main method
+          System.out.println("-----------");//11 dashes
+          System.out.println("| WELCOME |"); 
+          System.out.println("-----------");//11 dashes
+          
+          System.out.println("  ^  ^  ^  ^  ^  ^ "); 
+          System.out.println(" /\\ /\\ /\\ /\\ /\\ /\\ ");//must have a space between the slash and quotation marks
+          System.out.println("|-A--R--H--2--1--6-| ");
+          System.out.println(" \\/ \\/ \\/ \\/ \\/ \\/ ");
+          System.out.println("  v  v  v  v  v  v ");
+          System.out.println("This summer I worked at a tech company in the finance department");
+          System.out.println("I hope to work in investment banking in the future.");
+     }//end of method
+}//end of class

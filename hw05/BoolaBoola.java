5d883645ba62c777a7746d846009f2bb97bccdc5
arh216 <arh216@lehigh.edu>
2014-10-01 02:03:47 +0000

    updated BoolaBoola

---
+++ hw05/BoolaBoola.java
@@ -0,0 +1,12 @@
+//Ariana Hewitt
+//September 30, 2014
+//Homework 5
+//Program 2
+//Write a program that has three boolean variables 
+//to each of which the value of true or false is randomly assigned. 
+import java.util.Random;
+public class BoolaBoola {
+     public static void main(String[] args) {
+     
+     }//end of method
+}//end of class

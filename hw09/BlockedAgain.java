e8a98daa61f497b7f83f657345f0c4bc4a2cd2ce
arh216 <arh216@lehigh.edu>
2014-11-05 03:17:15 +0000

    updated BlockedAgain

---
+++ hw09/BlockedAgain.java
@@ -0,0 +1,13 @@
+//Ariana Hewitt
+//Homwework 9
+//
+
+import java.util.Scanner;
+public class BlockedAgain{
+     public static void main(String []s){
+          int m;
+//force user to enter int in range 1-9, inclusive.
+          m = getInt(); 
+          allBlocks(m);
+     }
+}
